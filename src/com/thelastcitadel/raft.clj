d7a3bc20feb0af83aa0b3dbd7bf333a8551ea435
Kevin Downey <redchin@gmail.com>
2014-05-17 10:29:31 -0700

    wip

--- src/com/thelastcitadel/raft.clj
+++ src/com/thelastcitadel/raft.clj
@@ -1,7 +1,28 @@
 (ns com.thelastcitadel.raft
-  (:require [clojure.core.async :refer [alt!! timeout >!]]
-            [clojure.core.match :refer [match]])
+  (:require [clojure.core.async :refer [alt!! timeout >!]])
   (:import (java.util UUID)))
+
+(defprotocol Cluster
+  (add-node [cluster node arg1])
+  (remove-node [cluster node])
+  (list-nodes [cluster])
+  (send-to [cluster node msg]))
+
+(defrecord ChannelCluster []
+  Cluster
+  (add-node [cluster node arg1]
+    (assoc cluster node arg1))
+  (remove-node [cluster node]
+    (dissoc cluster node))
+  (list-nodes [cluster]
+    (keys cluster))
+  (send-to [cluster node msg]
+    (assert (contains? cluster node))
+    (>! (get cluster node) msg)))
+
+(defn broadcast [cluster msg]
+  (doseq [node (list-nodes cluster)]
+    (send-to cluster node msg)))
 
 (defrecord RaftState [term voted-for log commit-index last-applied value])
 (defrecord LeaderState [next-index match-index])
@@ -14,7 +35,14 @@
   (append-entries [_ term leader-id prev-log-index prev-log-term entries leader-commit])
   (receive-vote-response [_ term vote-granted? node])
   (receive-append-entries-response [_ term success? node current-index])
-  (to-follower [_ term]))
+  (to-follower [_ term])
+  (prepare-heartbeat [_]))
+
+(defprotocol API
+  (delete [_ key])
+  (write [_ key value])
+  (read-op [_ key])
+  (write-if [_ key value]))
 
 ;; {:op :delete :args []}
 ;; {:op :write :args ["foo" "bar"]}
@@ -32,8 +60,8 @@
                      :delete (dissoc value (first args))
                      :write (assoc value (first args) (second args))
                      :read value
-                     :write-if (if (contains? state (first args))
-                                 state
+                     :write-if (if (contains? value (first args))
+                                 value
                                  (assoc value (first args) (second args)))))
                  (:value raft-state)
                  (:entries op))
@@ -42,6 +70,33 @@
                   :value value)]
       (recur state))
     raft-state))
+
+(defn last-log-index [raft-state]
+  (apply max 0 (keys raft-state)))
+
+(defn last-log-term [raft-state]
+  (:term (get (:log raft-state) (last-log-index raft-state))))
+
+(defn log-contains? [raft-state log-index log-term]
+  (and (contains? (:log raft-state) log-index)
+       (= log-term (get (:log raft-state) log-index))))
+
+(defn set-commit [raft-state leader-commit]
+  (if (> leader-commit (:commit-index raft-state))
+    (assoc raft-state
+      :commit-index (min leader-commit (last-log-index raft-state)))
+    raft-state))
+
+(defn append-log [raft-state entries]
+  (update-in raft-state [:log] (merge (into {} (for [entry entries] [(:index entry) entry])))))
+
+(defn clear-log-after [raft-state log-index]
+  (let [next-index (inc log-index)]
+    (if (contains? (:log raft-state) next-index)
+      (recur (update-in raft-state [:log] dissoc next-index) next-index)
+      raft-state)))
+
+(declare ->Candidate)
 
 (defrecord Follower [raft-state]
   Raft
@@ -53,7 +108,7 @@
                   :term term)))
   (leader? [_] false)
   (prepare-request-vote [_ candidate-id]
-    [(->Candidate raft-state votes)
+    [(->Candidate raft-state 0)
      {:term (:term raft-state)
       :candidate-id candidate-id
       :last-log-index (last-log-index raft-state)
@@ -88,11 +143,13 @@
        :current-index (last-log-index raft-state)
        :success? true}])))
 
+(declare ->Leader)
+
 (defrecord Candidate [raft-state votes]
   Raft
   (maintain [node]
     (->Candidate (maintain-raft-state raft-state)))
-  (leader [_] false)
+  (leader? [_] false)
   (to-follower [_ term]
     (->Follower (assoc raft-state
                   :voted-for nil
@@ -129,7 +186,7 @@
      [(->Candidate raft-state) {:term (:term raft-state)
                                 :vote-granted? false}]))
   (append-entries [node term leader-id prev-log-index prev-log-term entries leader-commit]
-    (append-entries (to-follower node) term leader-id prev-log-index prev-log-term entries leader-commit)))
+    (append-entries (to-follower node term) term leader-id prev-log-index prev-log-term entries leader-commit)))
 
 (defrecord Leader [raft-state raft-leader-state asserted?]
   Raft
@@ -151,26 +208,59 @@
   (request-vote [_ term candidate-id last-log-index last-log-term]
     [(->Leader raft-state) {:term (:term raft-state)
                             :vote-granted? false}])
-  (append-entries [_ term leader-id prev-log-index prev-log-term entries leader-commit]
-    [(->Leader raft-state) {:term (:term raft-state)
-                            :success? false}])
   (receive-append-entries-response [leader term success? follower current-index]
     (if success?
       (->Leader raft-state
                 (-> raft-leader-state
                     (assoc-in [:next-index follower] (inc current-index))
                     (assoc-in [:match-index follower] current-index)))
-      leader)))
+      leader))
+  (prepare-heartbeat [_]
+    {:term (:term raft-state)
+     :prev-log-index (last-log-index raft-state)
+     :prev-log-term (last-log-term raft-state)
+     :entries []
+     :leader-commit (:commit-index raft-state)})
+  API
+  (delete [_ key]
+    (let [op {:op :delete
+              :args [key]
+              :index (inc (last-log-index rift-state))}]
+      [(->Leader (-> raft-state
+                     (append-entries [op])))
+       op]))
+  (write [_ key value]
+    (let [op {:op :write
+              :args [key value]
+              :index (inc (last-log-index rift-state))}]
+      [(->Leader (-> raft-state
+                     (append-entries [op])))
+       op]))
+  (read-op [_ key]
+    (let [op {:op :read
+              :args [key]
+              :index (inc (last-log-index rift-state))}]
+      [(->Leader (-> raft-state
+                     (append-entries [op])))
+       op]))
+  (write-if [_ key value]
+    (let [op {:op :write-if
+              :args [key value]
+              :index (inc (last-log-index rift-state))}]
+      [(->Leader (-> raft-state
+                     (append-entries [op])))
+       op])))
 
+(def heart-beat-time-out 500)
 
 (defn run [node in cluster id]
   (let [to (if (leader? node)
              (* 0.75 heart-beat-time-out)
              heart-beat-time-out)]
-    (alts!!
+    (alt!!
      in ([message]
            (if (> (:term message) (:term (:raft-state node)))
-             #(run (to-follower node) in cluster id)
+             #(run (to-follower node (:term message)) in cluster id)
              (case (:type message)
                :request-vote (let [[new-node result] (request-vote node
                                                                    (:term message)
@@ -206,12 +296,13 @@
      (timeout to) ([_]
                      (if (leader? node)
                        (do
-                         (broadcast cluster (assoc (prepare-heartbeat node id)
+                         (broadcast cluster (assoc (prepare-heartbeat node)
                                               :from id
+                                              :leader-id id
                                               :type :append-entries))
                          #(run node in cluster id))
-                       (do
-                         (broadcast cluster (assoc (prepare-request-vote node id)
+                       (let [[new-node request] (prepare-request-vote node id)]
+                         (broadcast cluster (assoc request
                                               :from id
                                               :type :request-vote))
-                         #(run node in cluster id)))))))
+                         #(run new-node in cluster id)))))))
